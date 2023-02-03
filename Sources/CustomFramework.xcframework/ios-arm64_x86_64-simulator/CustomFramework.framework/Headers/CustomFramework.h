af0afbb9f0f3cfa307100ad5c85f34cc55025f9a
Wasi Zafar <wasi.zafar@upgenicsint.work>
2023-02-03 15:42:34 +0500

    custom framework is added

---
+++ Sources/CustomFramework.xcframework/ios-arm64_x86_64-simulator/CustomFramework.framework/Headers/CustomFramework.h
@@ -0,0 +1,18 @@
+//
+//  CustomFramework.h
+//  CustomFramework
+//
+//  Created by Shoaib@upgenics.int on 03/02/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomFramework.
+FOUNDATION_EXPORT double CustomFrameworkVersionNumber;
+
+//! Project version string for CustomFramework.
+FOUNDATION_EXPORT const unsigned char CustomFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomFramework/PublicHeader.h>
+
+
